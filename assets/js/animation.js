939d6af69a52a849a43d227d99ab6bee7335ac33
Sylvie <sylvie.z.lu@gmail.com>
2020-04-13 17:53:43 +0200

    responsive toggle + burger menu added

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,3 +1,4 @@
+// Script Toggle //
 const checkbox = document.querySelector('input[name=theme]');
 const currentTheme = localStorage.getItem('data-theme');
 
@@ -26,3 +27,33 @@ if (currentTheme) {
                 document.documentElement.classList.remove('transition')
             }, 1000)
         }
+// End Script Toggle //
+
+// Script Burger //
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    // Apparition menu //
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        // Apparition liens //
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFadeIn 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+            
+        });
+
+        // Animation burger menu //
+        burger.classList.toggle('burgerShut');
+    });
+}
+
+navSlide();
+
+// End Script Burger //
